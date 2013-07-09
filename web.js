d8a896f2509b698fd37e7dc27f367857a9ff0ab5
Ubuntu <ubuntu@ip-172-31-37-155.us-west-2.compute.internal>
2013-07-09 20:28:02 +0000

    essai

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf=fs.reafdFile('index.html');
+  buf=fs.reafdFile('./index.html');
 
   response.send(buf.toString('utf8',0,20);
 
