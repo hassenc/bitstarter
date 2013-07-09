d80f10c2d1e6716261cac54146ad3ba964e7441d
hassenc <hassen.chaieb@telecom-bretagne.eu>
2013-07-09 22:43:27 +0200

    Update web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = new Buffer(20);
-  buffer.write(fs.readFileSync("index.html", "utf-8"));
+  buffer=fs.readFileSync("index.html", "utf-8");
 
   response.send(buffer.toString('utf-8',0,20);
   //response.send('yo man');
