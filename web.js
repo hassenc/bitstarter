4c719f49149182b6d4c978780f718dbf25542d09
hassenc <hassen.chaieb@telecom-bretagne.eu>
2013-07-09 22:32:46 +0200

    Update web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf=fs.reafdFile('./index.html');
+  var buffer = new Buffer(20);
+  buffer.write(fs.reafdFile('./index.html'));
 
-  response.send(buf.toString('utf8',0,20);
+  response.send(buffer.toString('utf8',0,20);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
