c3bd0517d78fd0a4c85d23c7601f601cdfbe06f5
hassenc <hassen.chaieb@telecom-bretagne.eu>
2013-07-09 23:15:42 +0200

    Update web.js

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var fs=require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(16);
-  //buffer=fs.readFileSync("index.html", "utf-8");
-  buffer.write("Hello", "utf-8");
-  //response.send(buffer.toString('utf8',0 , 30);
-  response.send(buffer.toString('utf-8'));
+  var buffer = new Buffer("Hello World from index.html", 25);
+  buffer=fs.readFileSync("index.html", "utf-8");
+  //buffer.write("Hello", "utf-8");
+  response.send(buffer.toString('utf8',0 , 25;
+  //response.send(buffer.toString('utf-8'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
